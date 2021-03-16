8740081a4e639c2198e820ee1436475ccf5b1ff3
orange08160394 <80247040+orange08160394@users.noreply.github.com>
2021-03-16 11:37:16 +0800

    Add files via upload

---
+++ CameraScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+	void Update () {
+        // 根據鍵盤左右按鍵(水平移位)來旋轉
+		transform.Rotate(0,Input.GetAxis ("Horizontal"),0);
+        // 根據鍵盤上下按鍵(垂直移位)來前進/後退
+        transform.position+=transform.forward*Input.GetAxis("Vertical");
+	}
+}
---
+++ IndicatorScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorScript : MonoBehaviour { 
+    GameObject enemy;
+    void Start()
+    {   // 先根據 tag "Enemy" 找場景中的物件
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+
+   void Update()
+    {   // 讓方向指引對準 NPC 物件
+        transform.LookAt(enemy.transform);
+    }
+}
